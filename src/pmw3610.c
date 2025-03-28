c342bf6b758b8ca8a562108a9523a226c6687079
akiakirj1 <155696577+akiakirj1@users.noreply.github.com>
2025-03-29 05:19:55 +0900

    Update pmw3610.c

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -639,7 +639,7 @@ static int pmw3610_report_data(const struct device *dev) {
 
     float speed_multiplier = 1.0; //速度の倍率
     if (movement_size > 60) {
-        speed_multiplier = 3.2;
+        speed_multiplier = 3.4;
     }else if (movement_size > 30) {
         speed_multiplier = 1.5;
     }else if (movement_size > 5) {
